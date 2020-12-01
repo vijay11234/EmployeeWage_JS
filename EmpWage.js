15ede773e6411b700835b58f64ef423a781a50a2
vijay11234 <victory6923@gmail.com>
2020-12-01 17:37:45 +0530

    Employee wage for month

--- EmpWage.js
+++ EmpWage.js
@@ -1,12 +1,16 @@
 console.log("Welcome to employee wage problem");
 
-//UC3 
+//UC4
 
 const IS_PART_TIME = 2;
 const IS_FULL_TIME = 1;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+  let totalEmpHrs = 0;
+  let totalWorkingDays = 0;
 
 function getWorkingHours(empCheck){ 
     switch (empCheck) {
@@ -26,6 +30,10 @@ function getWorkingHours(empCheck){
       let empHrs = 0;
       empCheck = Math.floor(Math.random() * 10) % 3;
 
-      empHrs = getWorkingHours(empCheck);
-      let empWage = empHrs * WAGE_PER_HOUR;
-      console.log("emp Wage: " + empWage);
+      while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs +=  getWorkingHours(empCheck);
+     }
+        let empWage = totalEmpHrs * WAGE_PER_HOUR;
+        console.log("Total Days: " + totalWorkingDays + "Total Hrs: " +totalEmpHrs+ " Emp Wage: " + empWage);
