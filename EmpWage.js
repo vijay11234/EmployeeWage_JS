7528dfd38ef3c517ba607ab90b01402ee36da0c0
vijay11234 <victory6923@gmail.com>
2020-12-01 17:05:33 +0530

    Employee Daily wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,11 +1,30 @@
 console.log("Welcome to employee wage problem");
 
-//UC1 
+//UC2 
 
-const IS_ABSENTS = 0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENTS){
-    console.log("Is Absent");
-}else{
-    console.log("Is Present");
-}
+const IS_PART_TIME = 2;
+const IS_FULL_TIME = 1;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+   empCheck = Math.floor(Math.random() * 10) % 3;
+
+switch (empCheck) {
+        case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        console.log("Employee is Present");
+        break;
+
+        case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        console.log("Employee is Present");
+        break;
+        
+        default:
+         empHrs = 0;
+         console.log("Employee is Abscent");
+     }
+      let empWage = empHrs * WAGE_PER_HOUR;
+      console.log("emp Wage: " + empWage);
